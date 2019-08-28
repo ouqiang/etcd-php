ecf9ed49c865989754fa3460ed1901d509798325
isliang <yesuhuangsi@163.com>
2019-08-28 21:58:07 +0800

    remove code

--- src/Client.php
+++ src/Client.php
@@ -132,7 +132,6 @@ class Client
         ];
 
         $params = $this->encode($params);
-//        $options = $this->encode($options);
         $body = $this->request(self::URI_PUT, $params, $options);
         $body = $this->decodeBodyForFields(
             $body,
